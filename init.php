d429eb969eb01829205fc280fc9d756021575085
sentry360 <sentry360@gmail.com>
2019-06-15 00:26:38 -0400

    1st migration copy

---
+++ init.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+Route::set('media', 'media/<filepath>', array(
+		'filepath' => '.*', // Pattern to match the file path
+		'uid' => '.*?',     // Match the unique string that is not part of the media file
+	))
+	->defaults(array(
+		'controller' => 'media',
+		'action'     => 'serve',
+	));
+
+/*
+
+Route::set('media', 'media/(<uid>/)kohana/<filepath>', array(
+		'filepath' => '.*', // Pattern to match the file path
+		'uid' => '.*?',     // Match the unique string that is not part of the media file
+	))
+	->defaults(array(
+		'controller' => 'media',
+		'action'     => 'serve',
+	));
+ * 
+ * 
+ */
